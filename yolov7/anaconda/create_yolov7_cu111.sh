5f5df3d824256abcd81bbc5681c9ffef4838cd8d
fryuzoo <fryuzoo@gmail.com>
2023-01-11 13:37:30 +0900

    cudaを利用したバージョンのバグ修正

---
+++ yolov7/anaconda/create_yolov7_cu111.sh
@@ -0,0 +1,18 @@
+conda env create -f anaconda/yolov7_env.yml
+
+# >>> conda init >>>
+__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    \eval "$__conda_setup"
+else
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        \export PATH="$PATH:$HOME/anaconda3/bin"
+    fi
+fi
+unset __conda_setup
+
+conda activate yolov7
+pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
--- yolov7/anaconda/create_yolov7_env.sh
+++
@@ -1,5 +0,0 @@
-conda env create -f anaconda/yolov7_env.yml
-# source ~/anaconda3/etc/profile.d/conda.sh
-# conda init bash
-conda activate yolov7
-pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
